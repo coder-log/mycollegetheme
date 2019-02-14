bf806e2d7c59b2fbf051e38e181f6c1ebed8f9d5
Xavier <43478582+coder-log@users.noreply.github.com>
2019-02-14 08:56:12 +0100

    Updating header

--- page-past-events.php
+++ page-past-events.php
@@ -18,7 +18,6 @@
     $today = Date('Ymd');
     $pastEvents = new WP_Query(array(
         'paged'=>get_query_var('paged',1),
-        'posts_per_page'=>1,
         'post_type' => 'event',
         'meta_key'=>'event_date',
         'orderby'=>'meta_value_num',
